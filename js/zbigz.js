b0b37cc4614b57cf9ec556dc368d9ef18df526f0
GetUdemy <GetUdemy@users.noreply.github.com>
2015-12-13 02:01:38 +0700

    Create zbigz.js

---
+++ js/zbigz.js
@@ -0,0 +1,41 @@
+var objIframe = ''
+
+function decodeUrl(url){
+    url = (url + '===').slice(0, url.length + (url.length % 4));
+    return url.replace(/-/g, '+').replace(/_/g, '/');
+}
+
+function zbigz_insite(url, ref){
+  var container = document.createElement('div') 
+  container.innerHTML = '<iframe id="BruzzyDownloadWnd" src="//zbigz.com/insite/?tf='+encodeURIComponent(url)+'&ref='+ref+'"'+'style="position:fixed; left:0px; top:0px; width:100%; height:100%; z-index:999"'+'scrolling=0>'+'</iframe>'
+  var rezultHtml = container.firstChild
+  objIframe = rezultHtml
+  document.body.appendChild(rezultHtml)
+}
+
+var lastId = '';
+function checkForMessages(){
+if (location.hash != lastId){
+lastId = location.hash;
+lastHashIdx = lastId.lastIndexOf('#myMsg');
+if (lastHashIdx != -1) {
+// Пропарсить наш месадж
+msg = lastId.substring(lastHashIdx + 7); // lastHashIdx + strlen(’#myMsg’)
+
+// Убрать месадж из урла
+lastId = lastId.substring(0, lastHashIdx);
+location.hash = lastId;
+
+// Обработать месадж
+if (msg == 'close') {
+// TODO Handler here
+objIframe.parentNode.removeChild(objIframe)
+
+
+} else {
+    //alert('хз' + msg);
+}
+}
+}
+}
+setInterval(checkForMessages, 200);
